8f90dcb7de00b8100777d3f0b71863b650fa84d6
Abdulrahman92c <99719388+Abdulrahman92c@users.noreply.github.com>
2023-12-14 18:01:13 +0100

    modif test technique php et modif responsive

--- Test_tech_php/test_technique.php
+++ Test_tech_php/test_technique.php
@@ -5,18 +5,26 @@ echo $messyArray[0];
 
 //afficher Cerise 
 foreach($messyArray as $clé => $valeur){
-    if(is_array($valeur) && array_key_exists("Cerise",$valeur)){
-        echo $valeur["Cerise"];
-        break;
+    if(is_array($valeur) && array_key_exists("Cerise",$valeur)){ // la condition fait qu'on entre dans le if si Cerise existe dans le tableau
+
+        echo $valeur["Cerise"]; // donc ici on affiche Cerise après l'avoir trouvé 
+        break; // on sort de la boucle 
     }
 }
 
 //supprimer prune
 
-unset($messyArray[3][0]);
+foreach($messyArray as $clé => $valeur){
+    if(is_array($valeur) && in_array("Prune",$valeur)){ // la condition fait qu'on entre dans le if si on trouve Prune 
+
+        unset($messyArray[$clé][array_search("Prune",$valeur)]); // ici on supprime prune
+    }
+
+    print_r($messyArray); // on affiche donc le tableau sans prune
+}
 
 
-<!-- exercice 2 : -->
+// <!-- exercice 2 : -->
 
 $random_words = ["ape", "apple", "zoo", "pie", "elephant", "banana", "picnic", "eye"];
 
@@ -27,6 +35,7 @@ function comparaisonMots($a,$b){
     if($comparaisonlongueur == 0){ // ici on trie par ordre alphabetique si la longueur est la même
         return strcmp($a,$b);
     }
+
     return $comparaisonlongueur;
 
     usort($random_words,'comparaisonMots'); // ici se passe le tri de $randomwords à travers la fonction $comparaisonmots
@@ -36,13 +45,21 @@ function comparaisonMots($a,$b){
 
 
 
+// exercie 3
 
+class Calcul {
 
+public function removeDoublon($array){
+    return array_unique($array); //ici il supprime tous les doublons
+}
+}
+ $Calcul = new Calcul(); //on créer une instance de la class 
+ $nametb = ["Carole", "David", "Alice", "Bobby", "Bob", "David","Carole"]; // on initialise donc le tableau des prénoms avec les doublons
+ $nameWithoutDoublon =$Calcul->removeDoublon($nametb); // on appelle la methode removeDoublon pour retirer les doublons
 
+echo (implode(", ", $nameWithoutDoublon)); // affichage du tableau sans doublon
 
-
-
-
-
+ 
 
 ?>
+
